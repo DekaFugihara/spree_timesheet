e6eed37e92266f98272e2c31f21bfaa434f60695
Denise Fugihara <denise@retroca.com.br>
2016-10-25 09:57:08 -0200

    fix admin menu

--- app/overrides/banner_admin_tab.rb
+++ app/overrides/banner_admin_tab.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                       :name => "timesheet_admin_tab",
                       :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                      :text => "<ul class='nav nav-sidebar'><%= tab(:time_sheets, :icon => 'time', :label => 'Ponto', :url => spree.new_admin_time_sheet_path) %></ul>")
+                      :text => "<ul class='nav nav-sidebar'><%= tab 'Ponto', url: spree.new_admin_time_sheet_path, icon: 'time' %></ul>",
+                      :original => 'efc6f2099477786f7c0a90d890d990ad132d796b')
